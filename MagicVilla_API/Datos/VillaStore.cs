9f34650f23b0f30d127a729a01fcfb31bea5f060
Pierre <grandett1@gmail.com>
2023-03-21 22:13:34 -0500

    comentarios

--- MagicVilla_API/Datos/VillaStore.cs
+++ MagicVilla_API/Datos/VillaStore.cs
@@ -5,7 +5,7 @@ namespace MagicVilla_API.Datos
     public static class VillaStore
     {
         public static List<VillaDTO> villaList = new List<VillaDTO>
-        {
+        {// simulacion base de datos
             new VillaDTO{Id=1, Nombre="Vista a la Piscina"},
             new VillaDTO{Id=2, Nombre="Vista a la Playa"}
         };
--- MagicVilla_API/Modelos/VillaDTO/VillaDTO.cs
+++ MagicVilla_API/Modelos/VillaDTO/VillaDTO.cs
@@ -5,6 +5,7 @@ namespace MagicVilla_API.Modelos.VillaDTO
     public class VillaDTO
     {
         public int Id { get; set; }
+        //parametros obligatorios nombre
         [Required]
         [MaxLength(35)]
         public string Nombre { get; set; }
