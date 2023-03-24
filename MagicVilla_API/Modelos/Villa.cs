f156a851235b5486708c437cf059fedaf86c4692
Pierre <grandett1@gmail.com>
2023-03-23 20:38:57 -0500

    DataBase Connection

--- MagicVilla_API/Modelos/Villa.cs
+++ MagicVilla_API/Modelos/Villa.cs
@@ -1,10 +1,31 @@
-﻿namespace MagicVilla_API.Modelos
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MagicVilla_API.Modelos
 {     //PROPIEDADES
     public class Villa
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Nombre { get; set; }
+
+        public string Detalle { get; set; }
+       
+        [Required] 
+        public double Tarifa { get; set; }
+
+        public int Ocupantes { get; set; }
+
+        public int MetrosCuadrados { get; set; }
+
+        public string ImagenUrl { get; set; }
+
+        public string Amenidad { get; set; }
+          
         public    DateTime  FechaCreacion { get;set; }
+
+        public DateTime FechaActualizacion { get; set; }
         
       
     }
